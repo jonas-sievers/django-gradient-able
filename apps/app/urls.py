edb20c2a95528687625ccbfee73304be7d9f1115
jonas-sievers <petermeier9876@gmail.com>
2022-01-15 18:57:48 +0100

    restore login, regsiter

--- apps/app/urls.py
+++ apps/app/urls.py
@@ -9,7 +9,7 @@ from apps.app import views
 urlpatterns = [
 
     # The home page
-    path('', views.index, name='home'),
+    path('', views.index, name='welcome'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
